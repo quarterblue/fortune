630b41659b1af4bc99d107ad259a5357baa7bd56
Thomas Min <thomas_min@sfu.ca>
2021-03-19 18:22:05 -0700

    Initial commit, add client logic for sending arb msg to auth server

---
+++ client/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"net"
+)
+
+type ErrMessage struct {
+	Error string
+}
+
+type NonceMessage struct {
+	Nonce int64
+}
+
+type HashMessage struct {
+	Hash string
+}
+
+type FortuneInfoMessage struct {
+	FortuneServer string
+	FortuneNonce  int64
+}
+
+type FortuneReqMessage struct {
+	FortuneNonce uint64
+}
+
+type FortuneMessage struct {
+	Fortune string
+}
+
+// Hash function
+func newSHA256(data []byte) []byte {
+	hash := sha256.Sum256(data)
+	return hash[:]
+}
+
+func main() {
+
+	nonceReply := make([]byte, 1024)
+
+	conn, err := net.Dial("udp", ":1234")
+	if err != nil {
+		log.Fatal("Something went wrong:", err)
+		return
+	}
+
+	fmt.Fprintf(conn, "Arb Message")
+
+	_, err = bufio.NewReader(conn).Read(nonceReply)
+	fortNonce := &FortuneReqMessage{
+		FortuneNonce: binary.BigEndian.Uint64(nonceReply),
+	}
+
+	fmt.Printf("Nonce: %d\n", fortNonce.FortuneNonce)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/quarterblue/fortune
+
+go 1.16
