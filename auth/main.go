e5293a4dddbe68dd2784283eef26b4dceda9c266
Thomas Min <thomas_min@sfu.ca>
2021-03-19 19:47:24 -0700

    Auth server sends back nonce after receiving a msg fromt client

--- auth/main.go
+++ auth/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
 func main() {
 
+	arguments := os.Args
+
+	PORT := arguments[1]
+
+	s, err := net.ResolveUDPAddr("udp4", PORT)
+
+	conn, err := net.ListenUDP("udp4", s)
+	if err != nil {
+		log.Fatal("Connection failed:", err)
+	}
+
+	defer conn.Close()
+
+	var nonce int = 0
+
+	for {
+		buf := make([]byte, 1024)
+		n, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			log.Fatal("Error occured while reading buffer:", err)
+		}
+		fmt.Println("Recieved Arb Message:", string(buf[0:n-1]))
+
+		nonceToSend := []byte(strconv.Itoa(nonce))
+		nonce++
+		_, err = conn.WriteToUDP(nonceToSend, addr)
+		if err != nil {
+			log.Fatal("Error occured while sending message back:", err)
+			return
+		}
+
+	}
 }
