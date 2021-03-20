062ee52ddfe7a4ea562aed1bb83b4c6703f27606
Thomas Min <thomas_min@sfu.ca>
2021-03-19 19:30:17 -0700

    Hash nonce and secret

--- client/main.go
+++ client/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"bufio"
 	"crypto/sha256"
-	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 )
 
 type ErrMessage struct {
@@ -27,24 +28,36 @@ type FortuneInfoMessage struct {
 }
 
 type FortuneReqMessage struct {
-	FortuneNonce uint64
+	FortuneNonce int
 }
 
 type FortuneMessage struct {
 	Fortune string
 }
 
-// Hash function
-func newSHA256(data []byte, nonce uint64) []byte {
-	hash := sha256.Sum256(data)
+// Hash function to convert Nonce + Secret
+func newSHA256(nonce int, secret string) []byte {
+	nonceByte := []byte(strconv.Itoa(nonce))
+	bytestring := append(nonceByte, []byte(secret)...)
+	hash := sha256.Sum256(bytestring)
 	return hash[:]
 }
 
 func main() {
 
-	nonceReply := make([]byte, 1024)
+	arguments := os.Args[1:]
 
-	conn, err := net.Dial("udp", ":1234")
+	if len(arguments) != 3 {
+		fmt.Println("Usage: go run main.go [local UDP ip:port] [aserver ip:port] [secret]")
+		os.Exit(1)
+	}
+
+	localAddr := arguments[1]
+	aserverAddr := arguments[2]
+	secretMsg := arguments[3]
+
+	nonceReply := make([]byte, 1024)
+	conn, err := net.Dial("udp", aserverAddr)
 	if err != nil {
 		log.Fatal("Something went wrong:", err)
 		return
@@ -53,9 +66,15 @@ func main() {
 	fmt.Fprintf(conn, "Arb Message")
 
 	_, err = bufio.NewReader(conn).Read(nonceReply)
-	fortNonce := &FortuneReqMessage{
-		FortuneNonce: binary.BigEndian.Uint64(nonceReply),
+	byteToInt, err := strconv.Atoi(string(nonceReply))
+	if err != nil {
+		log.Fatal("Could not convert string ASCII to int: ", err)
 	}
+	fortNonce := &FortuneReqMessage{
+		FortuneNonce: byteToInt,
+	}
+
+	newHash := newSHA256(fortNonce.FortuneNonce, secretMsg)
 
 	fmt.Printf("Nonce: %d\n", fortNonce.FortuneNonce)
 }
