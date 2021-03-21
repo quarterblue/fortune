a0f20a75142872493f3773c7cfcb2212c2a7bde9
quarterblue <quarterslate@gmail.com>
2021-03-20 18:58:13 -0700

    Add mutex lock on nonce

--- auth/main.go
+++ auth/main.go
@@ -6,12 +6,26 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"sync"
 )
+
+type FortuneNonce struct {
+	mu    sync.Mutex
+	nonce int
+}
+
+// Gorountine function to read packets from UDP in separate threads
+func listen(conn *net.UDPConn, quit chan struct{}) {
+	buffer := make([]byte, 1024)
+	n, remoteAddr, err := 0, new(net.UDPAddr), error(nil)
+	for err == nil {
+		n, remoteAddr, err := conn.ReadFromUDP(buffer)
+	}
+}
 
 func main() {
 
 	arguments := os.Args
-
 	PORT := arguments[1]
 
 	s, err := net.ResolveUDPAddr("udp4", PORT)
@@ -23,7 +37,7 @@ func main() {
 
 	defer conn.Close()
 
-	var nonce int = 0
+	fortuneNonce := &FortuneNonce{nonce: 0}
 
 	for {
 		buf := make([]byte, 1024)
@@ -33,8 +47,13 @@ func main() {
 		}
 		fmt.Println("Recieved Arb Message:", string(buf[0:n-1]))
 
-		nonceToSend := []byte(strconv.Itoa(nonce))
-		nonce++
+		// Critical section start
+		fortuneNonce.mu.Lock()
+		nonceToSend := []byte(strconv.Itoa(fortuneNonce.nonce))
+		fortuneNonce.nonce++
+		fortuneNonce.mu.Unlock()
+		// Critical section end
+
 		_, err = conn.WriteToUDP(nonceToSend, addr)
 		if err != nil {
 			log.Fatal("Error occured while sending message back:", err)
@@ -42,4 +61,5 @@ func main() {
 		}
 
 	}
+
 }
